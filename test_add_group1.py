15b4138ae23d3171a0c0b0e5b972137a11187082
pzalewski <pzalewski@argo.pl>
2018-04-26 15:17:23 +0200

    fixed task number 4

--- test_add_group1.py
+++ test_add_group1.py
@@ -7,7 +7,7 @@ from application import Application
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinlizer(fixture.destroy)
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_add_group1(app):
