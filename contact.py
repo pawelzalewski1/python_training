a0ac7bd1156e21a617501f4508cb3bd50e38b6d5
pzalewski <pzalewski@argo.pl>
2018-04-26 09:38:32 +0200

    adding contact.py

---
+++ contact.py
@@ -0,0 +1,6 @@
+class Contact:
+    def __init__(self, firstname, lastname,address,email):
+        self.firstname=firstname
+        self.lastname=lastname
+        self.address=address
+        self.email=email
