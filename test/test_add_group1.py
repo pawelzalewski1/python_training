b6020857c5b5583f20ba0dc826d2df4a03979927
pzalewski <pzalewski@argo.pl>
2018-04-27 09:17:14 +0200

    before adding group in fixture

--- test/test_add_group1.py
+++ test/test_add_group1.py
@@ -5,7 +5,6 @@ import pytest
 from fixture.application import Application
 from model.group import Group
 
-
 @pytest.fixture
 def app(request):
     fixture = Application()
@@ -13,11 +12,11 @@ def app(request):
     return fixture
 
 def test_add_group1(app):
-        app.login(username="admin")
+        app.session.login(username="admin")
         app.create_group(Group(name="ddd", header="dddd", footer="ddd"))
-        app.logout()
+        app.session.logout()
 
 def test_add_empty_group1(app):
-        app.login(username="admin")
+        app.session.login(username="admin")
         app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+        app.session.logout()
