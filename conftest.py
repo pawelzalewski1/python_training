fe83b77a63735d30cfac232d433687181c82cf29
pzalewski <pzalewski@argo.pl>
2018-04-29 14:46:01 +0200

    C:\Devel\python_training\env\Scripts\env\Scripts\python.exe "C:\Program Files\JetBrains\PyCharm Community Edition 2017.1.7\helpers\pydev\pydevd.py" --multiproc --qt-support --client 127.0.0.1 --port 52672 --file C:/Devel/python_training/conftest.py pydev debugger: process 7076 is connecting
    Connected to pydev debugger (build 171.4694.87)
    
    Process finished with exit code 0

--- conftest.py
+++ conftest.py
@@ -1,19 +1,10 @@
-from fixture.application import Application
+# -*- coding: utf-8 -*-
+
 import pytest
-from fixture.application_contact import ApllicationContact
+from fixture.application import Application
 
-#@pytest.fixture(scope = "session")
-#def app(request):
- #   fixture = Application()
-  #  request.addfinalizer(fixture.destroy)
-   # return fixture
-#??????????????
-#@pytest.fixture(scope = "session_contact")
-
-### z filmu:
 @pytest.fixture
 def app(request):
-  #  fixture = ApllicationContact()
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
