9cf98ffef339d8e435b2173657aa209d9c44b88e
pzalewski <pzalewski@argo.pl>
2018-04-30 15:15:02 +0200

    task 7

---
+++ test/test_add_group.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="ssss", header="dddd", footer="fffff"))
+    app.session.logout()
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
