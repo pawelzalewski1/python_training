3d19428bee3569120e3766e4b0593002f8afbc33
pzalewski <pzalewski@argo.pl>
2018-05-07 15:19:13 +0200

    problem with test delete contact

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,5 +1,16 @@
+# from model.contact import Contact
+#
+# def test_delete_first_contact(app):
+#     if app.contact.count()==0:
+#         app.contact.create(Contact(first_name="test"))
+#     app.contact.delete_first_contact()
+
+
+
+
+
+
+
 
 def test_delete_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.delete_first_contact()
-    app.session.logout()
